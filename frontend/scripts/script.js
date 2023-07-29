c190887b245926750b71e11bc01a2d2a911f6d8e
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-30 00:13:29 +0300

    added register fetch

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -30,6 +30,47 @@ pages.login = () => {
     })
     }
 
+pages.signup = () => {
+    const name_input = document.getElementById('name-input')
+    const email_input = document.getElementById('email-input-signup')
+    const password_input = document.getElementById('password-input-signup')
+    const verify_password_input = document.getElementById('password-input-verify')
+    const signup_btn = document.getElementById('signup-btn')
+    
+    signup_btn.addEventListener('click', e => {
+        e.preventDefault()
+        const name = name_input.value
+        const email = email_input.value
+        const password = password_input.value
+        const verify_password = verify_password_input.value
+        if(email && password && verify_password && email && name ){
+            if (password == verify_password){
+                const signup_form_data = new FormData();
+                signup_form_data.append("name", name);
+                signup_form_data.append("email", email);
+                signup_form_data.append("password", password);
+                
+                fetch(pages.base_url + "register", {
+                    method: 'POST',
+                    body: signup_form_data,
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if(data.message == 'User created successfully'){
+                            console.log('User created successfully')
+                        }
+                    })
+                    .catch(error => console.log('error', error));
+            } else {
+                console.log('passwords don\'t match.')
+            }
+            
+            }else{
+                console.log('fill all please.')
+            }
+    })
+    }
+
 
 
 pages.productCardHover = () =>{
